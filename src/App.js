358a590c6d105b888d5d94135c70498dd075b084
Mohanraj-Muthukumaran <mohanraj.muthukumaran@rxglobal.com>
2023-02-26 18:15:42 +0530

    Add. navbar and footer component to all pages

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
 import Products from './pages/Products';
+import Error from './pages/Error';
 
 function App() {
   return (
     <BrowserRouter>
+        <nav>
+            our navbar
+        </nav>
         <Routes>
+            <Route path='*' element={ <Error/> }/>
             <Route path='/' element={ <Home/> }/>
             <Route path='/about' element={ <About/> }/>
             <Route path='/products' element={ <Products/> }/>
         </Routes>
+        <footer>
+          our footer
+        </footer>
     </BrowserRouter>
   );
 }
